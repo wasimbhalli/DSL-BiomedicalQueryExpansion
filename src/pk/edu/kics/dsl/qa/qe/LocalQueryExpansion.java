d7341f2f43f889b8c99f94da584d32471d40b1c4
Muhammad Wasim <cmwasim@gmail.com>
2017-09-12 13:19:29 +0500

    modified loop size from fixed ten documents to flexible number of documents retrieved

--- src/pk/edu/kics/dsl/qa/qe/LocalQueryExpansion.java
+++ src/pk/edu/kics/dsl/qa/qe/LocalQueryExpansion.java
@@ -99,7 +99,7 @@ public class LocalQueryExpansion extends QueryExpansion{
 			}
 		}
 
-		tdMatrix = new Array2DRowRealMatrix(localDictionary.size(), BiomedQA.DOCUMENTS_FOR_QE);
+		tdMatrix = new Array2DRowRealMatrix(localDictionary.size(), resultsList.size());
 
 		for(int docIndex=0; docIndex<resultsList.size(); docIndex++) {
 			HashMap<String,Integer> TermsFrequency = (HashMap<String,Integer>) documentTermFrequencies.get(docIndex);
