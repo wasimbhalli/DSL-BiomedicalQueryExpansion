edb5dd8a26881a968c1f9864a88f87e6765629dc
Muhammad Wasim <cmwasim@gmail.com>
2017-09-21 21:11:57 +0500

    added new configurations to accomodate semantic filtering

--- src/pk/edu/kics/dsl/qa/BiomedQA.java
+++ src/pk/edu/kics/dsl/qa/BiomedQA.java
@@ -2,6 +2,7 @@ package pk.edu.kics.dsl.qa;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Map;
 
 import org.apache.solr.client.solrj.SolrServerException;
@@ -15,6 +16,7 @@ import pk.edu.kics.dsl.qa.util.CollectionHelper;
 import pk.edu.kics.dsl.qa.util.CombHelper;
 import pk.edu.kics.dsl.qa.util.Evaluation;
 import pk.edu.kics.dsl.qa.util.IOHelper;
+import pk.edu.kics.dsl.qa.util.SimilarityHelper;
 import pk.edu.kics.dsl.qa.util.SolrHelper;
 import pk.edu.kics.dsl.qa.util.StringHelper;
 
@@ -22,26 +24,36 @@ public class BiomedQA {
 
 	private enum Combination {
 		Intersection, 
-		Broda, 
+		Borda, 
 		Linear // Only two techniques will be used for this combination
 	};
 
-	private final static Boolean isCombination = true;
-	private final static Combination combinationTechnique = Combination.Linear;
-	private final static double linearAlpha = 0.6;
-	
+	public static enum SemanticSource {
+		MeSH, 
+		WordEmbedding
+	};
+
+	private final static Boolean COMBINATION_ENABLED = false;
+	private final static Combination COMBINATION_TECHNIQUE = Combination.Borda;
+	private final static double LINEAR_ALPHA = 0.6;
+
 	// If no technique is to be used, use "NQ" as QE_TECHNIQUE which means no Query Expansion
-	private final static String[] QE_TECHNIQUES = {"MFT", "LRF"};
+	private final static String[] QE_TECHNIQUES = {"MFT"};
 
 	// IR_MODEL also needs to be changed in core's managed-scheme file to work.
 	//private final static String IR_MODEL = ""; 
 
-	public final static int DOCUMENTS_FOR_QE = 10;
-	private final static int TOP_TERMS_TO_SELECT = 10;
+	public final static int DOCUMENTS_FOR_QE = 5;
+	public final static int TOP_TERMS_TO_SELECT = 5;
 	public final static boolean DISPLAY_RESULTS = true;
 
 	public final static boolean STEMMING_ENABLED = false;
-	
+
+	// only applicable for individual feature selection technique - not for combinations
+	public final static SemanticSource SEMANTIC_SOURCE_TECHNIQUE = SemanticSource.MeSH;
+	public final static boolean SEMANTIC_FILTERING_ENABLED = true;
+	public final static int TOP_TERMS_FOR_SEMANTIC_FILTERING = 10;
+
 	private final static String QUESTIONS_PATH = "resources/2007topics.txt";
 	public final static String SOLR_SERVER = "localhost";
 	public final static String SOLR_CORE = "genomic_html";
@@ -56,21 +68,21 @@ public class BiomedQA {
 		try {
 			String experiment = "";
 
-			if(!isCombination) {
+			if(!COMBINATION_ENABLED) {
 				for (int i = 0; i < QE_TECHNIQUES.length; i++) {
 					experiment = QE_TECHNIQUES[i];// + "-" + IR_MODEL;
 					IOHelper.deletePreviousResults();
 					processAllQuestions(questionsList, QE_TECHNIQUES[i]);
 					Evaluation.evaluateResults(experiment);
-					
+
 					System.out.println("Done: " + experiment);
 				}
 			} else {
-				experiment = combinationTechnique.toString() + "-" + String.join(",", QE_TECHNIQUES);;
+				experiment = COMBINATION_TECHNIQUE.toString() + "-" + String.join(",", QE_TECHNIQUES);;
 				IOHelper.deletePreviousResults();
 				processAllQuestions(questionsList, null);
 				Evaluation.evaluateResults(experiment);
-				
+
 				System.out.println("Done: " + experiment);
 			}
 
@@ -83,7 +95,7 @@ public class BiomedQA {
 	private static void processAllQuestions(ArrayList<Question> questionsList, String qeTechnique) throws Exception {
 		int counter = 1;
 		for (Question question : questionsList) {
-			processQuestion(question, qeTechnique, counter++);
+			processQuestion(question, qeTechnique, counter++);break;
 		}
 	}
 
@@ -94,18 +106,27 @@ public class BiomedQA {
 		String relevantTerms = "";
 		String queryWords = question.getQuestion();
 
-		if(!isCombination) {
+		if(!COMBINATION_ENABLED) {
 			if(!qeTechnique.toLowerCase().equals("nq")) {
 				String qeClass = "pk.edu.kics.dsl.qa.qe." + qeTechnique;
 				QueryExpansion qe = (QueryExpansion) Class.forName(qeClass).newInstance();
 				Map<String, Double> sortedTerms = qe.getRelevantTerms(question);
-				relevantTerms=  CollectionHelper.getTopTerms(sortedTerms, TOP_TERMS_TO_SELECT);
-				queryWords = StringHelper.mergeTerms(queryWords, relevantTerms);
+
+				if(SEMANTIC_FILTERING_ENABLED) {
+					relevantTerms = SimilarityHelper.applySemanticFiltering(
+							sortedTerms, StringHelper.solrPreprocessor(queryWords), SEMANTIC_SOURCE_TECHNIQUE);
+				} else {
+					relevantTerms =  CollectionHelper.getTopTerms(sortedTerms, TOP_TERMS_TO_SELECT);
+				}
+
+				if(relevantTerms!=null && !relevantTerms.isEmpty()) {
+					queryWords = StringHelper.mergeTerms(queryWords, relevantTerms);
+				}
 			}	
 		} else {
 			ArrayList<String> terms = new ArrayList<>();
 			ArrayList<Map<String, Double>> lists = new ArrayList<>();
-			
+
 			for (int i = 0; i < QE_TECHNIQUES.length; i++) {
 				String technique = QE_TECHNIQUES[i];
 				String qeClass = "pk.edu.kics.dsl.qa.qe." + technique;
@@ -115,20 +136,20 @@ public class BiomedQA {
 				relevantTerms=  CollectionHelper.getTopTerms(sortedTerms, TOP_TERMS_TO_SELECT);
 				terms.add(relevantTerms);
 			}
-			
-			if(combinationTechnique == Combination.Intersection) {
+
+			if(COMBINATION_TECHNIQUE == Combination.Intersection) {
 				relevantTerms = CombHelper.intersect(terms);
-			} else if(combinationTechnique == Combination.Linear) {
-				Map<String, Double> sortedTerms = CombHelper.linear(lists, linearAlpha);
+			} else if(COMBINATION_TECHNIQUE == Combination.Linear) {
+				Map<String, Double> sortedTerms = CombHelper.linear(lists, LINEAR_ALPHA);
 				relevantTerms=  CollectionHelper.getTopTerms(sortedTerms, TOP_TERMS_TO_SELECT);
-			} else if(combinationTechnique == Combination.Broda) {
+			} else if(COMBINATION_TECHNIQUE == Combination.Borda) {
 				Map<String, Double> sortedTerms = CombHelper.borda(terms);
 				relevantTerms=  CollectionHelper.getTopTerms(sortedTerms, TOP_TERMS_TO_SELECT);
 			}
-			
+
 			queryWords = StringHelper.mergeTerms(queryWords, relevantTerms);
 		}
-		
+
 
 		ArrayList<String> finalQuery = StringHelper.solrPreprocessor(queryWords);
 
